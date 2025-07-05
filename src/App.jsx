3211f94df3f3eb85c1473a82fa796143c9db764a
Fahim Faysal <mfaysal223224@bscse.uiu.ac.bd>
2025-07-05 19:50:56 +0600

    A basic counter app created with redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CounterView  from './features/counter/counterView.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     App
+     <h1>Counter App</h1>
+     <CounterView/>
     </>
   )
 }
