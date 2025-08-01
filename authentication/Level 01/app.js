ce7b591e8e37d1b11512d187d84f31c83f93aa13
Fahim Faysal <mfaysal223224@bscse.uiu.ac.bd>
2025-08-01 18:16:54 +0600

    authentication level one complete

---
+++ authentication/Level 01/app.js
@@ -0,0 +1,99 @@
+// match from database authentication
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/user.model");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const dbURL = process.env.MONGO_URL;
+
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(dbURL)
+  .then(() => {
+    console.log("mongodb atlas is connected");
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/./views/index.html");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (user && user.password === password) {
+      res.status(200).json({ status: "valid user" });
+    } else {
+      res.status(404).json({ status: "Not valid user" });
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+// route not found error
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "route not found",
+  });
+});
+
+
+
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+//handling server error
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: "something broke",
+  });
+});
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+});
