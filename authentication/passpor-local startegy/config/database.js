ab19a08d0c1b22373cd3ce3199fc14a31ca9469b
Fahim Faysal <mfaysal223224@bscse.uiu.ac.bd>
2025-08-07 21:19:45 +0600

    passport-locl startegy has been added

--- authentication/passpor-local startegy/config/database.js
+++ authentication/passpor-local startegy/config/database.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log("db is connected");
+    console.log("database connection is established");
   })
   .catch((error) => {
     console.log(error.message);
